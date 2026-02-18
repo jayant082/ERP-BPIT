04236df091c63a9877dc7c0b0cacd127d6c4c85e
jayant0802 <jayantmohan15@gmail.com>
2026-02-19 01:32:51 +0530

    fixed deployment issues and updated API URL for production

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.js
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 
 import { AuthProvider } from './context/AuthContext';
 import { StudentDataProvider } from './context/StudentDataContext';
--- client/vite.config.js
+++ client/vite.config.js
@@ -2,13 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/ERP-BPIT/",
   plugins: [
     react(),
     tailwindcss(),
   ],
-  server:{
-    port:3000,
-  }
+  
 })
